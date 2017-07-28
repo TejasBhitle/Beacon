a3361de01fa35c41a160c207bec63afdc7576677
TejasBhitle <bhitletejas@gmail.com>
2017-07-29 00:46:54 +0530

    Presentation App

--- app/src/main/java/com/codeblooded/beacon/activities/ItemDetailActivity.java
+++ app/src/main/java/com/codeblooded/beacon/activities/ItemDetailActivity.java
@@ -1,46 +1,92 @@
 package com.codeblooded.beacon.activities;
 
+import android.animation.Animator;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.support.v4.view.animation.LinearOutSlowInInterpolator;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.transition.Slide;
+import android.transition.Transition;
+import android.view.Gravity;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.view.ViewAnimationUtils;
 
 import com.codeblooded.beacon.R;
-import com.codeblooded.beacon.model.Item;
 
 /**
  * Created by tejas on 7/26/17.
  */
 
-public class ItemDetailActivity extends AppCompatActivity{
+public class ItemDetailActivity extends AppCompatActivity {
 
-    TextView sampleTextView;
+    @Override
+    protected void onStart() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Slide slide = new Slide(Gravity.BOTTOM);
+
+            slide.addTarget(R.id.card1);
+            slide.addTarget(R.id.card2);
+            slide.setInterpolator(new LinearOutSlowInInterpolator());
+            getWindow().setEnterTransition(slide);
+            getWindow().setExitTransition(slide);
+            getWindow().setReenterTransition(slide);
+
+        }
+        super.onStart();
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void show(View view,long delay) {
+        // previously invisible view
+        int centerX = (view.getLeft() + view.getRight()) / 2;
+        int centerY = (view.getTop() + view.getBottom()) / 2;
+
+        int startRadius = 0;
+        int endRadius = Math.max(view.getWidth(), view.getHeight());
+
+        Animator anim = ViewAnimationUtils.createCircularReveal(view, centerX, centerY, startRadius, endRadius);
+        anim.setDuration(delay);
+        view.setVisibility(View.VISIBLE);
+        anim.start();
+    }
+
+    //TextView sampleTextView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_detail);
 
-        setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        setTitle("");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(getResources().getDrawable(R.drawable.ic_close_white_24dp));
 
-        sampleTextView = (TextView)findViewById(R.id.sampleTextView);
+        transition();
+    }
 
-        Bundle bundle = getIntent().getExtras();
-        if(bundle != null){
-            Item item = bundle.getParcelable("item");
-            if(item!=null) {
-                sampleTextView.setText(item.getName());
-            }
-        }
-
-
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void transition(){
+        new Handler().postDelayed(
+                new Runnable(){
+                    @Override
+                    public void run() {
+                        show(findViewById(R.id.content1),750);
+                        show(findViewById(R.id.content2),1000);
+                        show(findViewById(R.id.content3),1250);
+                    }
+                },500
+        );
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case android.R.id.home:
                 onBackPressed();
                 return true;
---
+++ app/src/main/java/com/codeblooded/beacon/activities/tempactivites/SearchActivity.java
@@ -0,0 +1,45 @@
+package com.codeblooded.beacon.activities.tempactivites;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.codeblooded.beacon.R;
+import com.codeblooded.beacon.activities.ItemDetailActivity;
+
+/**
+ * Created by tejas on 7/28/17.
+ */
+
+public class SearchActivity extends AppCompatActivity {
+
+    ProgressDialog progressDialog;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        progressDialog = new ProgressDialog(SearchActivity.this);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressDialog.setMessage("Searching...");
+        progressDialog.show();
+        new Handler().postDelayed(
+                new Runnable(){
+                    @Override
+                    public void run() {
+                        progressDialog.cancel();
+                        startActivity(new Intent(SearchActivity.this, ItemDetailActivity.class));
+                        overridePendingTransition(R.anim.silde_in_up,R.anim.do_nothing);
+                    }
+                },2000
+        );
+    }
+}
